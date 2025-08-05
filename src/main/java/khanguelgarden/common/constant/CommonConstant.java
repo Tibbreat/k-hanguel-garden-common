b5d7dac60d99434ea1b34c2bb0a21b94e60fb18d
KienNT <kiennt7902@gmail.com>
2025-08-05 17:10:21 +0700

    add Swagger, common constant

--- src/main/java/khanguelgarden/common/constant/CommonConstant.java
+++ src/main/java/khanguelgarden/common/constant/CommonConstant.java
@@ -6,6 +6,9 @@ public class CommonConstant {
 
     public static final String DATE_FORMAT = "dd-MM-yyyy";
     public static final String DATE_TIME_FORMAT = "dd-MM-yyyy HH:mm:ss";
+    public static final String TIME_ZONE = "Asia/Ho_Chi_Minh";
 
-
+//    Authentication constants
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String AUTHORIZATION_HEADER_PREFIX = "Bearer ";
 }
